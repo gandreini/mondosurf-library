dea05fff82c8e527b93764eb26dfcaea2aded13a
Giulio Andreini <g.andreini@gmail.com>
2023-08-31 08:39:05 +0200

    Removed IAP debug stuff from library.

--- helpers/debug.helpers.ts
+++ helpers/debug.helpers.ts
@@ -1,19 +1,3 @@
-import { IAPProduct } from "@ionic-native/in-app-purchase-2";
-
-import { ExtendedTransaction } from "../model/ExtendedIAPTransaction";
-
-/**
- * Shows the main details of a IAP product in a readable way.
- * Used only for debugging.
- *
- * @param   {IAPProduct} product Product to show the details.
- * @returns {string} Details of the product in a readable way.
- */
-export const showProduct = (product: IAPProduct) => {
-    const transaction: ExtendedTransaction = product.transaction as ExtendedTransaction;
-    return "id: " + product.id + ", title: " + product.title + ", state: " + product.state + ", owned: " + product.owned + ", transaction: " + JSON.stringify(transaction) + ", original_transaction_id: " + transaction.original_transaction_id + ", expiration date: " + transaction.expiration_date + ", account upgrade:" + transaction.account_upgrade;
-}
-
 /**
  * Return true if the debug is active.
  *
