afa5647e578a0e67df39c97b9855bd70636bea18
Giulio Andreini <g.andreini@gmail.com>
2023-09-04 07:37:45 +0200

    New Client helpers file.

--- helpers/device.helpers.ts
+++ helpers/device.helpers.ts
@@ -1,5 +1,3 @@
-import { DeviceUUID } from 'device-uuid';
-
 /**
  * Check the with of the device and returns true if it's bigger that the provided minWidth.
  *
@@ -38,41 +36,4 @@ export function isMobile(): boolean {
  */
 export function getPlatform(): "web" {
     return "web";
-}
-
-/**
- * Checks if there's a device_id in the localStorage.
- * If there's no device_id, it creates one and returns it.
- * If there's already a device_id, it returns it.
- * The device_id is a unique identifier of the device.
- * 
- * @returns {string} The unique id of the device.
- */
-export const getDeviceId = (): string => {
-    if (!localStorage.getItem('device_id')) {
-        const du = new DeviceUUID().parse();
-        const dua = [
-            du.platform,
-            du.os,
-            du.cpuCores,
-            du.isDesktop,
-            du.isMobile,
-            du.isTablet,
-            du.isWindows,
-            du.isLinux,
-            du.isLinux64,
-            du.isMac,
-            du.isiPad,
-            du.isiPhone,
-            du.isiPod,
-            du.isSmartTV,
-            du.pixelDepth,
-            du.isTouchScreen
-        ];
-        const uuid = du.hashMD5(dua.join(':'));
-        localStorage.setItem('device_id', uuid);
-        return uuid;
-    } else {
-        return localStorage.getItem('device_id')!;
-    }
 }
