3779e1a66cabe8bba11570525838ae447a863e81
Giulio Andreini <g.andreini@gmail.com>
2023-09-19 07:32:01 +0200

    Some stuff.

---
+++ components/Metadata.tsx
@@ -0,0 +1,24 @@
+interface IMetadata {
+    label?: string;
+    value: string;
+    inline?: boolean;
+}
+
+const Metadata: React.FC<IMetadata> = (props) => {
+    const classes = () => {
+        let className = 'ms-metadata';
+        if (props.inline) {
+            className += ' ms-metadata__inline ';
+        }
+        return className;
+    };
+
+    return (
+        <div className={classes()}>
+            {props.label && <p className="ms-metadata__label">{props.label}</p>}
+            <p className="ms-metadata__value">{props.value}</p>
+        </div>
+    );
+};
+
+export default Metadata;
