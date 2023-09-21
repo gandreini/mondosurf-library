14efec9e8defd9211937a4dec209be2e1332c1e1
Giulio Andreini <g.andreini@gmail.com>
2023-09-21 08:41:19 +0200

    Improved empy state.

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,7 +1,14 @@
+import Button from '@/components/Button';
+
 interface IEmptyState {
     emoji?: string;
     title?: string;
     text?: string;
+    buttonLabel?: string;
+    buttonUrl?: string;
+    buttonStyle?: 'cta' | 'light' | 'normal';
+    buttonSize?: 'xl' | 'l' | 'm' | 's';
+    buttonCallback?: () => void;
 }
 
 const EmptyState: React.FC<IEmptyState> = (props) => {
@@ -10,6 +17,22 @@ const EmptyState: React.FC<IEmptyState> = (props) => {
             {props.emoji && <p className="ms-empty-state__emoji ms-emoji">{props.emoji}</p>}
             {props.title && <p className="ms-empty-state__title ms-emoji">{props.title}</p>}
             {props.text && <p className="ms-empty-state__text ms-body-text">{props.text}</p>}
+            {props.buttonLabel && props.buttonCallback && (
+                <Button
+                    label={props.buttonLabel}
+                    callback={props.buttonCallback}
+                    size={props.buttonSize ? props.buttonSize : 'm'}
+                    style={props.buttonStyle ? props.buttonStyle : 'normal'}
+                />
+            )}
+            {props.buttonLabel && props.buttonUrl && (
+                <Button
+                    label={props.buttonLabel}
+                    url={props.buttonUrl}
+                    size={props.buttonSize ? props.buttonSize : 'm'}
+                    style={props.buttonStyle ? props.buttonStyle : 'normal'}
+                />
+            )}
         </div>
     );
 };
