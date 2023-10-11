2c35d70ff12464131429a0bbfd1975d4992ddb2a
Giulio Andreini <g.andreini@gmail.com>
2023-10-11 08:05:09 +0200

    New library components.

---
+++ components/ChartDirectionMini.tsx
@@ -0,0 +1,117 @@
+import { COLOR_VARIABLES } from '@/mondosurf-library/constants/colorVariables';
+
+interface IChartDirectionMini {
+    direction: string[];
+    directionColor: string;
+    bgColor?: string;
+}
+
+const ChartDirectionMini: React.FC<IChartDirectionMini> = (props) => {
+    const bgColor = props.bgColor ? props.bgColor : COLOR_VARIABLES.cssColorGray03;
+    return (
+        <svg height="20" width="20" viewBox="0 0 20 20">
+            <circle r="10" cx="10" cy="10" fill={bgColor} />
+            {props.direction.includes('NE') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 292.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('N') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 247.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('NW') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 202.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('W') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 157.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('SW') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 112.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('S') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 67.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('SE') && (
+                <circle
+                    r="5"
+                    cx="10"
+                    cy="10"
+                    fill="transparent"
+                    stroke={props.directionColor}
+                    strokeWidth="9"
+                    strokeDasharray="0 calc( 22.5 * 31.42 / 360 ) calc( 45 * 31.42 / 360 ) 31.42"
+                />
+            )}
+            {props.direction.includes('E') && (
+                <>
+                    <circle
+                        r="5"
+                        cx="10"
+                        cy="10"
+                        fill="transparent"
+                        stroke={props.directionColor}
+                        strokeWidth="9"
+                        strokeDasharray="0 calc( 0 * 31.42 / 360 ) calc( 22.5 * 31.42 / 360 ) 31.42"
+                    />
+                    <circle
+                        r="5"
+                        cx="10"
+                        cy="10"
+                        fill="transparent"
+                        stroke={props.directionColor}
+                        strokeWidth="9"
+                        strokeDasharray="0 calc( 337.5 * 31.42 / 360 ) calc( 22.5 * 31.42 / 360 ) 31.42"
+                    />
+                </>
+            )}
+            <circle r="9" cx="10" cy="10" strokeWidth="0" stroke={props.directionColor} fill="transparent"></circle>
+        </svg>
+    );
+};
+export default ChartDirectionMini;
---
+++ components/SurfSpotDirection.tsx
@@ -0,0 +1,37 @@
+import { COLOR_VARIABLES } from '@/mondosurf-library/constants/colorVariables';
+import ChartDirectionMini from '@/mondosurf-library/components/ChartDirectionMini';
+import { readableDirectionFromDegrees } from '@/features/surfSpot/surfSpot.helpers';
+
+interface ISurfSpotDirection {
+    min: number;
+    max: number;
+    label: string;
+    type: 'swell' | 'wind';
+    hideLabel?: boolean;
+}
+
+const SurfSpotDirection: React.FC<ISurfSpotDirection> = (props: ISurfSpotDirection) => {
+    const returnDirectionColor = () => {
+        if (props.type === 'swell') return COLOR_VARIABLES.cssColorSwell;
+        if (props.type === 'wind') return COLOR_VARIABLES.cssColorWind;
+        return COLOR_VARIABLES.cssColorGray06;
+    };
+
+    return (
+        <div className={`ms-surf-spot-direction ms-surf-spot-direction-${props.type}`}>
+            <div className="ms-surf-spot-direction__chart">
+                <ChartDirectionMini
+                    direction={readableDirectionFromDegrees(props.min, props.max)}
+                    directionColor={returnDirectionColor()}
+                />
+            </div>
+            <div className="ms-surf-spot-direction__text">
+                {!props.hideLabel && <div className="ms-surf-spot-direction__label">{props.label}</div>}
+                <div className="ms-surf-spot-direction__value">
+                    {readableDirectionFromDegrees(props.min, props.max).join(', ')}
+                </div>
+            </div>
+        </div>
+    );
+};
+export default SurfSpotDirection;
