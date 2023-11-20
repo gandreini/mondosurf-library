008bcd0105da9e708e2f9a676a08bf8952e9bfe3
Giulio Andreini <andreini@netseven.it>
2023-11-20 08:16:07 +0100

    Password reset related.

--- model/loginModalContext.ts
+++ model/loginModalContext.ts
@@ -1,1 +1,1 @@
-export type LoginModalContext = 'header' | 'profilePage' | 'subscriptionConfirmed' | 'tabBar' | 'homeProCta' | 'proButton' | 'favoriteAddButton' | 'calendar';
+export type LoginModalContext = 'header' | 'profilePage' | 'subscriptionConfirmed' | 'tabBar' | 'homeProCta' | 'proButton' | 'favoriteAddButton' | 'calendar' | 'passwordReset';
