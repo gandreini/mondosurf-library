9a2894d6bd01934be455db52e57f5f957ce82c1a
Giulio Andreini <andreini@netseven.it>
2024-04-04 18:38:28 +0200

    Auth stuff moved.

---
+++ auth/AuthTokenExpiryCheck.tsx
@@ -0,0 +1,37 @@
+import { refreshToken } from 'features/auth/auth.helpers';
+import jwt_decode from 'jwt-decode';
+import IAccessToken from 'mondosurf-library/model/iAccessToken';
+import { RootState } from 'mondosurf-library/redux/store';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+const AuthTokenExpiryCheck: React.FC = (props) => {
+    const tokenRefreshInterval: number = parseInt(process.env.REACT_APP_REFRESH_TOKEN_INTERVAL_SECONDS!) * 1000;
+    const logged = useSelector((state: RootState) => state.user.logged);
+    const accessToken = useSelector((state: RootState) => state.user.accessToken);
+    const [count, setCount] = useState<number>(0);
+    const deviceId = useSelector((state: RootState) => state.appConfig.device_id);
+
+    useEffect(() => {
+        if (accessToken && logged === 'yes') {
+            const timer = setTimeout(() => {
+                setCount((c) => c + 1);
+                checkToken();
+            }, tokenRefreshInterval);
+        }
+        // return () => clearTimeout(timer);
+    }, [count, logged]);
+
+    const checkToken = () => {
+        if (accessToken && logged === 'yes') {
+            const decodedToken: IAccessToken = jwt_decode(accessToken);
+            if (Math.floor(Date.now() / 1000) > decodedToken.exp) {
+                refreshToken(accessToken, deviceId);
+            }
+        }
+    };
+
+    return <></>;
+};
+export default AuthTokenExpiryCheck;
---
+++ components/LogoutButton.tsx
@@ -0,0 +1,34 @@
+// Client
+'use client';
+
+import { revoke } from 'features/auth/auth.helpers';
+import Button from 'mondosurf-library/components/Button';
+import { RootState } from 'mondosurf-library/redux/store';
+import { mondoTranslate } from 'proxies/mondoTranslate';
+import { useSelector } from 'react-redux';
+
+interface ILogoutButton {
+    parentFunction?: () => void;
+}
+
+const LogoutButton: React.FC<ILogoutButton> = (props) => {
+    // Redux.
+    const accessToken = useSelector((state: RootState) => state.user.accessToken);
+    const deviceId = useSelector((state: RootState) => state.appConfig.device_id);
+
+    // Revokes the token and logs out the user
+    const onLogout = () => {
+        revoke(accessToken, deviceId)
+            .then((response) => {
+                if (response && response.data.success === true) {
+                    if (props.parentFunction) props.parentFunction();
+                }
+            })
+            .catch(function (error) {
+                // What happens if there's a logout error? We try to logout the user anyway:
+                if (props.parentFunction) props.parentFunction();
+            });
+    };
+    return <Button label={mondoTranslate('basics.logout')} style="cta" size="l" callback={onLogout} />;
+};
+export default LogoutButton;
