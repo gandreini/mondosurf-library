cb0775c4bcf68930bd268993e9650e7112b0f429
Giulio Andreini <g.andreini@gmail.com>
2024-09-01 17:40:18 +0200

    Version update

--- constants/constants.ts
+++ constants/constants.ts
@@ -9,10 +9,10 @@ export const STRIPE_MONTHLY_SUBSCRIPTION_CANCEL_URL = "monthly-subscription-canc
 
 export const YEARLY_SUBSCRIPTION_TITLE = "Mondo Surf Pro Yearly";
 export const MONTHLY_SUBSCRIPTION_TITLE = "Mondo Surf Pro Monthly";
-export const STRIPE_YEARLY_SUBSCRIPTION_PRICE = "€ 29.99";
-export const STRIPE_MONTHLY_SUBSCRIPTION_PRICE = "€ 2.99";
-export const IOS_DEFAULT_YEARLY_SUBSCRIPTION_PRICE = "€ 39.99"; // Used when the real price hasn't been loaded yet.
-export const IOS_DEFAULT_MONTHLY_SUBSCRIPTION_PRICE = "€ 3.99"; // Used when the real price hasn't been loaded yet.
+export const STRIPE_YEARLY_SUBSCRIPTION_PRICE = "€ 19.99";
+export const STRIPE_MONTHLY_SUBSCRIPTION_PRICE = "€ 1.99";
+export const IOS_DEFAULT_YEARLY_SUBSCRIPTION_PRICE = "€ 19.99"; // Used when the real price hasn't been loaded yet.
+export const IOS_DEFAULT_MONTHLY_SUBSCRIPTION_PRICE = "€ 1.99"; // Used when the real price hasn't been loaded yet.
 
 export const FREE_USER_MAX_FORECAST_DAYS = 3;
 export const PRO_USER_MAX_FORECAST_DAYS = 7;
@@ -22,5 +22,5 @@ export const PRIVACY_POLICY_URL = "https://www.iubenda.com/privacy-policy/508515
 export const IOS_EULA_URL = "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/";
 export const MAIN_CONTACT_EMAIL = "hello@mondo.surf";
 
-export const APP_LATEST_VERSION = "4.7.0";
-export const APP_LATEST_VERSION_PROGRESSIVE = 54;
+export const APP_LATEST_VERSION = "4.8.0";
+export const APP_LATEST_VERSION_PROGRESSIVE = 55;
