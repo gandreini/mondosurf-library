8bf3266687ac2a7fa06345c3bd60b4fe15d36b1c
Giulio Andreini <g.andreini@gmail.com>
2025-09-25 08:27:54 +0200

    Version number update.

--- constants/constants.ts
+++ constants/constants.ts
@@ -26,5 +26,5 @@ export const MAIN_CONTACT_EMAIL = "hello@mondo.surf";
 
 export const GEOJSON_FILE_URL = "https://rest-api.mondo.surf/wp-content/uploads/geojson/worldwide-spots.json"
 
-export const APP_LATEST_VERSION = "4.14.0";
-export const APP_LATEST_VERSION_PROGRESSIVE = 63;
+export const APP_LATEST_VERSION = "4.15.0";
+export const APP_LATEST_VERSION_PROGRESSIVE = 65;
