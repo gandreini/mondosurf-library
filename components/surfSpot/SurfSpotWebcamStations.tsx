19cd4b26c6425d9db5f4732d49c38335110921bf
Giulio Andreini <g.andreini@gmail.com>
2026-01-27 07:54:56 +0100

    Move hr element into SurfSpotWebcamStations, render only when content exists

--- components/surfSpot/SurfSpotWebcamStations.tsx
+++ components/surfSpot/SurfSpotWebcamStations.tsx
@@ -8,32 +8,44 @@ interface ISurfSpotWebcamStations {
 }
 
 const SurfSpotWebcamStations: React.FC<ISurfSpotWebcamStations> = (props) => {
-    return (
-        <div className="ms-surf-spot-webcam-stations">
-            {/* Webcams */}
-            {!props.spot.hide_location && props.spot.webcams && props.spot.webcams.length > 0 && (
-                <section className="ms-surf-spot-webcam-stations__section">
-                    <h2 className="ms-h3-title">{mondoTranslate('surf_spot.webcams')}</h2>
-                    <div className="ms-grid-2-4">
-                        {props.spot.webcams.map((item: ISurfSpotWebcam, key: number) => (
-                            <SurfSpotWebcam key={key} url={item.url} name={item.name} thumb={item.thumb} />
-                        ))}
-                    </div>
-                </section>
-            )}
+    const hasWebcams = !props.spot.hide_location && props.spot.webcams && props.spot.webcams.length > 0;
+    const hasMeteoStations =
+        !props.spot.hide_location && props.spot.meteo_stations && props.spot.meteo_stations.length > 0;
+    const hasContent = hasWebcams || hasMeteoStations;
 
-            {/* Meteo Stations */}
-            {!props.spot.hide_location && props.spot.meteo_stations && props.spot.meteo_stations.length > 0 && (
-                <section className="ms-surf-spot-webcam-stations__section">
-                    <h2 className="ms-h3-title">{mondoTranslate('surf_spot.meteo_stations')}</h2>
-                    <div className="ms-grid-2-4">
-                        {props.spot.meteo_stations.map((item: ISurfSpotMeteoStation, key: number) => (
-                            <SurfSpotMeteoStation key={key} url={item.url} name={item.name} thumb={item.thumb} />
-                        ))}
-                    </div>
-                </section>
-            )}
-        </div>
+    if (!hasContent) {
+        return null;
+    }
+
+    return (
+        <>
+            <hr />
+            <div className="ms-surf-spot-webcam-stations">
+                {/* Webcams */}
+                {hasWebcams && (
+                    <section className="ms-surf-spot-webcam-stations__section">
+                        <h2 className="ms-h3-title">{mondoTranslate('surf_spot.webcams')}</h2>
+                        <div className="ms-grid-2-4">
+                            {props.spot.webcams!.map((item: ISurfSpotWebcam) => (
+                                <SurfSpotWebcam key={item.url} url={item.url} name={item.name} thumb={item.thumb} />
+                            ))}
+                        </div>
+                    </section>
+                )}
+
+                {/* Meteo Stations */}
+                {hasMeteoStations && (
+                    <section className="ms-surf-spot-webcam-stations__section">
+                        <h2 className="ms-h3-title">{mondoTranslate('surf_spot.meteo_stations')}</h2>
+                        <div className="ms-grid-2-4">
+                            {props.spot.meteo_stations!.map((item: ISurfSpotMeteoStation) => (
+                                <SurfSpotMeteoStation key={item.url} url={item.url} name={item.name} thumb={item.thumb} />
+                            ))}
+                        </div>
+                    </section>
+                )}
+            </div>
+        </>
     );
 };
 export default SurfSpotWebcamStations;
