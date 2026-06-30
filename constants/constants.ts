fa4408a7edf5b9697582c5e178cd5fa01dbbcc87
Giulio Andreini <g.andreini@gmail.com>
2026-06-30 18:20:16 +0200

    Version updates.

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,28 +2,28 @@ export const TOAST_DURATION = 2800;
 export const FORECAST_UPDATE_PERIOD = 21600; // 6 hours in seconds
 export const FORECAST_UPDATES = [4, 10, 16, 22];
 
-export const STRIPE_YEARLY_SUBSCRIPTION_SUCCESS_URL = "yearly-subscription-success";
-export const STRIPE_YEARLY_SUBSCRIPTION_CANCEL_URL = "yearly-subscription-cancel";
-export const STRIPE_MONTHLY_SUBSCRIPTION_SUCCESS_URL = "monthly-subscription-success";
-export const STRIPE_MONTHLY_SUBSCRIPTION_CANCEL_URL = "monthly-subscription-cancel";
+export const STRIPE_YEARLY_SUBSCRIPTION_SUCCESS_URL = 'yearly-subscription-success';
+export const STRIPE_YEARLY_SUBSCRIPTION_CANCEL_URL = 'yearly-subscription-cancel';
+export const STRIPE_MONTHLY_SUBSCRIPTION_SUCCESS_URL = 'monthly-subscription-success';
+export const STRIPE_MONTHLY_SUBSCRIPTION_CANCEL_URL = 'monthly-subscription-cancel';
 
-export const YEARLY_SUBSCRIPTION_TITLE = "Mondo Pro Yearly";
-export const MONTHLY_SUBSCRIPTION_TITLE = "Mondo Pro Monthly";
-export const STRIPE_YEARLY_SUBSCRIPTION_PRICE = "€ 19.99";
-export const STRIPE_MONTHLY_SUBSCRIPTION_PRICE = "€ 1.99";
-export const IOS_DEFAULT_YEARLY_SUBSCRIPTION_PRICE = "€ 19.99"; // Used when the real price hasn't been loaded yet.
-export const IOS_DEFAULT_MONTHLY_SUBSCRIPTION_PRICE = "€ 1.99"; // Used when the real price hasn't been loaded yet.
+export const YEARLY_SUBSCRIPTION_TITLE = 'Mondo Pro Yearly';
+export const MONTHLY_SUBSCRIPTION_TITLE = 'Mondo Pro Monthly';
+export const STRIPE_YEARLY_SUBSCRIPTION_PRICE = '€ 19.99';
+export const STRIPE_MONTHLY_SUBSCRIPTION_PRICE = '€ 1.99';
+export const IOS_DEFAULT_YEARLY_SUBSCRIPTION_PRICE = '€ 19.99'; // Used when the real price hasn't been loaded yet.
+export const IOS_DEFAULT_MONTHLY_SUBSCRIPTION_PRICE = '€ 1.99'; // Used when the real price hasn't been loaded yet.
 
 export const MAX_FORECAST_DAYS = 8;
 
 // export const TERMS_CONDITIONS_URL = "https://www.iubenda.com/terms-and-conditions/50851546";
 // export const PRIVACY_POLICY_URL = "https://www.iubenda.com/privacy-policy/50851546/legal";
-export const TERMS_CONDITIONS_URL = "https://www.mondo.surf/terms-conditions";
-export const PRIVACY_POLICY_URL = "https://www.mondo.surf/privacy-policy";
-export const IOS_EULA_URL = "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/";
-export const MAIN_CONTACT_EMAIL = "hello@mondo.surf";
+export const TERMS_CONDITIONS_URL = 'https://www.mondo.surf/terms-conditions';
+export const PRIVACY_POLICY_URL = 'https://www.mondo.surf/privacy-policy';
+export const IOS_EULA_URL = 'https://www.apple.com/legal/internet-services/itunes/dev/stdeula/';
+export const MAIN_CONTACT_EMAIL = 'hello@mondo.surf';
 
-export const GEOJSON_FILE_URL = "https://rest-api.mondo.surf/wp-content/uploads/geojson/worldwide-spots.json"
+export const GEOJSON_FILE_URL = 'https://rest-api.mondo.surf/wp-content/uploads/geojson/worldwide-spots.json';
 
-export const APP_LATEST_VERSION = "4.17.0";
-export const APP_LATEST_VERSION_PROGRESSIVE = 69;
+export const APP_LATEST_VERSION = '4.19.0';
+export const APP_LATEST_VERSION_PROGRESSIVE = 71;
